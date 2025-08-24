c10e309044c398194a348cea5de209b7785f1422
Brogan Pratt <101632496+MrPrattASH@users.noreply.github.com>
2025-08-24 18:31:12 +0200

    Update LinearSlides.java
    production ready code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LinearSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LinearSlides.java
@@ -3,4 +3,14 @@ package org.firstinspires.ftc.teamcode.mechanisms;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class LinearSlides extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        
+    }
 }
